ea874a485c317fc4fb96ecfad6f5a9d9d3a09556
Fazil Fahad <fazil@timeless.co>
2016-09-17 19:47:38 +0530

    about page completed

--- js/main.js
+++ js/main.js
@@ -173,40 +173,7 @@ $(document).ready(function() {
         jQuery('.smooth-scroll').scrollingTo();
     }());
 
-    function tabEffectsContrl() {
-        tabSlide();
-
-        $('.nav-tabs li').on('shown.bs.tab', function() {
-            $('#magic-line').remove();
-            tabSlide();
-        });
-
-        function tabSlide() {
-            $("#tabControl").append("<li id='magic-line'></li>");
-            var $magicLine = $("#magic-line");
-            $magicLine
-                .width($(".active").width())
-                .css("left", $(".active a").position().left)
-                .data("origLeft", $magicLine.position().left)
-                .data("origWidth", $magicLine.width());
-            $("#tabControl li").find("a").hover(function() {
-                $el = $(this);
-                leftPos = $el.position().left;
-                newWidth = $el.parent().width();
-                $magicLine.stop().animate({
-                    left: leftPos,
-                    width: newWidth
-                });
-            }, function() {
-                $magicLine.stop().animate({
-                    left: $magicLine.data("origLeft"),
-                    width: $magicLine.data("origWidth")
-                });
-            });
-
-        }
-    }
-    tabEffectsContrl();
+    
 
 
     function categoryControl() {
@@ -215,11 +182,6 @@ $(document).ready(function() {
         console.log("selectedCategory::" + selectedCategory);
     }
     // categoryControl();
-
-    function okayControl() {
-        var navigation = $('#categoryMenuControl').okayNav();
-    }
-    okayControl();
 
 
     // resize
